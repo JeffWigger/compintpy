78cded11a4816cda784bca600b4d9da1737e0c6a
Jeffrey Wigger <wiggerjeffrey@gmail.com>
2023-04-16 13:03:30 +0200

    sucessfully linking compc

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,21 @@
 #include <pybind11/pybind11.h>
 
+#include <compc/elias_gamma.hpp>
 #define STRINGIFY(x) #x
 #define MACRO_STRINGIFY(x) STRINGIFY(x)
 
 int add(int i, int j) {
+    std::size_t size = 10;
+    long input[10] =  {1, 3, 2000, 2, 50, 1,25345, 11, 10000000, 1};
+    compc::EliasGamma<long> elias;
+    std::cout << "size: " << sizeof(input) << std::endl;
+    uint8_t* comp = elias.compress(input, size);
+    std::cout << "compressed" << std::endl;
+    long* output = elias.decompress(comp);
+    std::cout << "decompress" << std::endl;
+    for(int i = 0; i < 10; i++){
+        std::cout << output[i] << input[i] << std::endl; // comparing values
+    }
     return i + j;
 }
 
