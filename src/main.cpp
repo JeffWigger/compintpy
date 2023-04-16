3060bc002424ec243dc801f678740659b6c14bf3
Jeffrey Wigger <wiggerjeffrey@gmail.com>
2023-04-16 14:34:29 +0200

    adding compress and decompress functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,51 +1,59 @@
 #include <pybind11/pybind11.h>
-
+#include <pybind11/numpy.h>
 #include <compc/elias_gamma.hpp>
 #define STRINGIFY(x) #x
 #define MACRO_STRINGIFY(x) STRINGIFY(x)
 
-int add(int i, int j) {
-    std::size_t size = 10;
-    long input[10] =  {1, 3, 2000, 2, 50, 1,25345, 11, 10000000, 1};
-    compc::EliasGamma<long> elias;
-    std::cout << "size: " << sizeof(input) << std::endl;
-    uint8_t* comp = elias.compress(input, size);
-    std::cout << "compressed" << std::endl;
-    long* output = elias.decompress(comp);
-    std::cout << "decompress" << std::endl;
-    for(int i = 0; i < 10; i++){
-        std::cout << output[i] << input[i] << std::endl; // comparing values
-    }
-    return i + j;
-}
 
 namespace py = pybind11;
 
+py::array_t<uint8_t> compress(py::array_t<uint32_t> array){
+  compc::EliasGamma<uint32_t> elias;
+  const ssize_t* sizes = array.shape();
+  std::size_t N = sizes[0];
+  uint8_t* comp = elias.compress(array.data(), N);
+  auto capsule = py::capsule(comp, [](void *v) {
+    //std::cerr << "freeing memory @ " << v << std::endl;
+    delete v;
+  });
+  return py::array(10, comp, capsule);
+  
+}
+
+
+py::array_t<uint32_t> decompress(py::array_t<uint8_t> array){
+
+  compc::EliasGamma<uint32_t> elias;
+  const ssize_t* sizes = array.shape();
+  std::size_t N = sizes[0];
+  uint32_t* decomp = elias.decompress(array.data());
+  auto capsule = py::capsule(decomp, [](void *v) {
+    //std::cerr << "freeing memory @ " << v << std::endl;
+    delete v;
+  });
+  return py::array(10, decomp, capsule);
+}
+
 PYBIND11_MODULE(comppy, m) {
     m.doc() = R"pbdoc(
-        Pybind11 example plugin
+        Fast Variable Length Encodings For Python
         -----------------------
-
-        .. currentmodule:: comppy
-
+        .. currentmodule:: fastvarints
         .. autosummary::
            :toctree: _generate
-
-           add
-           subtract
+           compress
+           decompress
     )pbdoc";
 
-    m.def("add", &add, R"pbdoc(
-        Add two numbers
 
-        Some other explanation about the add function.
+    m.def("compress", &compress, R"pbdoc(
+        compresses a numpy array using the elias gamma encoding
     )pbdoc");
 
-    m.def("subtract", [](int i, int j) { return i - j; }, R"pbdoc(
-        Subtract two numbers
-
-        Some other explanation about the subtract function.
+    m.def("decompress", &decompress, R"pbdoc(
+        decompresses a numpy byte array with elias gamma encoded numbers
     )pbdoc");
+
 
 #ifdef VERSION_INFO
     m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
